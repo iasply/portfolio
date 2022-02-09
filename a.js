675f265af28ad419eac02cd3cc9541adb9b75331
iasply <iuryasilva@gmail.com>
2022-02-09 10:41:18 -0300

    main

---
+++ a.js
@@ -0,0 +1,17 @@
+const num = [1,2,3,4,5,6,7,8,9,0]
+
+// create a phone number
+
+function createPhoneNumber(numbers){
+  let a =0
+  let b = []
+  while (a < 10) {
+    if (a == 0){
+      b.
+
+    }
+    a
+  }
+}
+
+createPhoneNumber(num)
